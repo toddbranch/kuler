a2b53d5bd990806d479923e92749c8074d188787
Todd Branchflower <todd.branchflower@gmail.com>
2014-08-28 10:59:29 -0600

    created model dependencies, validations.  added slug generation logic after save

--- app/models/color.rb
+++ app/models/color.rb
@@ -1,2 +1,5 @@
 class Color < ActiveRecord::Base
+	belongs_to		:palette
+
+	validates			:hex_value, presence: true
 end
--- app/models/palette.rb
+++ app/models/palette.rb
@@ -1,2 +1,13 @@
 class Palette < ActiveRecord::Base
+	has_many		:colors, dependent: :destroy
+
+	validates		:name, presence: true, uniqueness: true
+
+	after_save	:generate_slug
+
+	private
+
+		def generate_slug
+			self.slug = "#{self.id} #{self.name}".parameterize
+		end
 end
