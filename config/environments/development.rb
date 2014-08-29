07c9803caed30b5c3de81f3e977ba403293f2ba1
Todd Branchflower <todd.branchflower@gmail.com>
2014-08-29 11:43:40 -0600

    created share mailer, configured action mailer, created email views

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,17 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+
+	ActionMailer::Base.smtp_settings = {
+		:address        => "smtp.gmail.com",
+		:port           => 587,
+		:domain         => "gmail.com",
+		:user_name      => "todd.branchflower@gmail.com",
+		:password       => "SumNJ079*&",
+		:authentication => :plain
+	}
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
