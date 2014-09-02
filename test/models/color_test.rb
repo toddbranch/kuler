861c874efd66072b8c553a211d2f00cd7832f6e0
Todd Branchflower <todd.branchflower@gmail.com>
2014-09-02 17:17:51 -0600

    added fixtures and tests for color model

--- test/models/color_test.rb
+++ test/models/color_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class ColorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "should not save a color without a hex value" do
+		color = Color.new
+		assert_not color.save
+	end
+
+	test "should save a color with a duplicate hex value" do
+		color = Color.new
+		color.hex_value = "ABCDEF"
+		assert color.save, "did not save a color with a duplicate hex value"
+	end
+
+	test "should not save a color with a hex value length that's not 6 chars" do
+		color = Color.new
+		color.hex_value = "ABCD"
+		assert_not color.save, "saved a color with a hex value of only 4 chars"
+	end
 end
