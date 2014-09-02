5c8ca1403b06de1b73cecabe0435e677b50e4da0
Todd Branchflower <todd.branchflower@gmail.com>
2014-09-02 16:39:27 -0600

    edited mail config to deliver to file vice through gmail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,15 +16,16 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
+	config.action_mailer.delivery_method = :file
 
-	ActionMailer::Base.smtp_settings = {
-		:address        => "smtp.gmail.com",
-		:port           => 587,
-		:domain         => "gmail.com",
-		:user_name      => "todd.branchflower@gmail.com",
-		:password       => "SumNJ079*&",
-		:authentication => :plain
-	}
+	# ActionMailer::Base.smtp_settings = {
+		# :address        => "smtp.gmail.com",
+		# :port           => 587,
+		# :domain         => "gmail.com",
+		# :user_name      => "todd.branchflower@gmail.com",
+		# :password       => "",
+		# :authentication => :plain
+	# }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
