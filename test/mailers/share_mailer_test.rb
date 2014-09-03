5c6f5c0ed07d6535af58557561d463596771ceba
Todd Branchflower <todd.branchflower@gmail.com>
2014-09-02 18:03:52 -0600

    added assertions for mailer test

--- test/mailers/share_mailer_test.rb
+++ test/mailers/share_mailer_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class ShareMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "share" do
+		palette = Palette.first
+		share_email = "todd.branchflower@gmail.com"
+		host = "http://localhost:3000"
+		email = ShareMailer.share_email(palette, share_email, host).deliver
+
+		assert_not ActionMailer::Base.deliveries.empty?
+		
+		assert_equal [share_email], email.to
+		assert_equal 'Check out Palette 1', email.subject
+	end
 end
