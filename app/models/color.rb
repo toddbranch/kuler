29d2350d5b6084530e40a95c30dc02010085815f
Todd Branchflower <todd.branchflower@gmail.com>
2014-09-03 09:14:58 -0600

    added format validation to color

--- app/models/color.rb
+++ app/models/color.rb
@@ -1,5 +1,9 @@
 class Color < ActiveRecord::Base
   belongs_to    :palette
 
-  validates     :hex_value, presence: true, length: { is: 6 }
+  validates     :hex_value, 
+                presence: true, 
+                length: { is: 7 },
+                format: { with: /\A#[0-9A-Fa-f]+\z/ }
+
 end
