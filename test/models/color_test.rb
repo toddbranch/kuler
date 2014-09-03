20ed0db322993d2b44c7b89663204de11e6f66bc
Todd Branchflower <todd.branchflower@gmail.com>
2014-09-03 09:15:39 -0600

    added / modified color tests to align with new format validation

--- test/models/color_test.rb
+++ test/models/color_test.rb
@@ -8,13 +8,25 @@ class ColorTest < ActiveSupport::TestCase
 
   test "should save a color with a duplicate hex value" do
     color = Color.new
-    color.hex_value = "ABCDEF"
+    color.hex_value = "#ABCDEF"
     assert color.save, "did not save a color with a duplicate hex value"
   end
 
-  test "should not save a color with a hex value length that's not 6 chars" do
+  test "should save a color with a valid hex value" do
+    color = Color.new
+    color.hex_value = "#abc112"
+    assert color.save, "did not save a color with a valid hex value"
+  end
+
+  test "should not save a color with a hex value length that's not 7 chars" do
     color = Color.new
     color.hex_value = "ABCD"
     assert_not color.save, "saved a color with a hex value of only 4 chars"
+  end
+
+  test "should not save a color with an invalid format " do
+    color = Color.new
+    color.hex_value = "ABCDEF#"
+    assert_not color.save, "saved a color with an invalid format"
   end
 end
